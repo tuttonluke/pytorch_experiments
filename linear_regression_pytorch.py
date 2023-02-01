abeb7cda1c2e8bc3e200e1ce38d451327449af7b
Luke Tutton <tuttonluke@gmail.com>
2023-02-01 17:20:34 +0000

    Adds multiclass classification example.

--- linear_regression_pytorch.py
+++ linear_regression_pytorch.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
-from sklearn.datasets import load_diabetes, make_classification
+from sklearn.datasets import load_diabetes, make_classification, load_iris
 # %%
 class DiabetesDataset(Dataset):
     def __init__(self) -> None:
@@ -31,6 +31,17 @@ class BinaryClassificationDataset(Dataset):
     def __len__(self):
         return len(self.X)
 
+class IrisDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.X, self.y = load_iris(return_X_y=True)
+    
+    def __getitem__(self, index):
+        return (torch.tensor(self.X[index]).float(), torch.tensor(self.y[index]).float())
+    
+    def __len__(self):
+        return len(self.X)
+
 class LinearRegression(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -48,6 +59,14 @@ class LogisticRegression(torch.nn.Module):
         
     def forward(self, features):
         return torch.sigmoid(self.linear_layer(features))
+
+class MulticlassClassification(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.linear_layer = torch.nn.Linear(4, 3)
+    
+    def forward(self, features):
+        return F.softmax(self.linear_layer(features))
 # %% 
 def regression_train(model, epochs=10):
     optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
@@ -67,7 +86,7 @@ def regression_train(model, epochs=10):
             writer.add_scalar(tag="Loss", scalar_value=loss.item(), global_step=batch_index)
             batch_index += 1
 
-def classification_train(model, epochs=10):
+def binary_classification_train(model, epochs=10):
     optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
     # initialise tensorboard visualiser
     writer = SummaryWriter()
@@ -78,6 +97,24 @@ def classification_train(model, epochs=10):
             features, labels = batch
             prediction = model(features)
             loss = F.binary_cross_entropy(prediction, labels)
+            loss.backward() # populates grad attributes
+            print(loss.item())
+            optimiser.step() # optimisation step
+            optimiser.zero_grad() # reset gradients
+            writer.add_scalar(tag="Loss", scalar_value=loss.item(), global_step=batch_index)
+            batch_index += 1
+
+def multiclass_classification_train(model, epochs=10):
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
+    # initialise tensorboard visualiser
+    writer = SummaryWriter()
+    batch_index = 0
+
+    for epoch in range(epochs):
+        for batch in train_loader:
+            features, labels = batch
+            prediction = model(features)
+            loss = F.cross_entropy(prediction, labels.long())
             loss.backward() # populates grad attributes
             print(loss.item())
             optimiser.step() # optimisation step
@@ -95,5 +132,10 @@ dataset = BinaryClassificationDataset()
 train_loader = DataLoader(dataset, shuffle=True, batch_size=4)
 model = LogisticRegression()
 
-classification_train(model)
-# %%
+binary_classification_train(model)
+# %% MULTICLASS CLASSIFICATION EXAMPLE 
+dataset = IrisDataset()
+train_loader = DataLoader(dataset, shuffle=True, batch_size=4)
+model = MulticlassClassification()
+
+multiclass_classification_train(model)
