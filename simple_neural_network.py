e7e03603800a1e8fa0c61724ac5bb590b4750179
Luke Tutton <tuttonluke@gmail.com>
2023-02-01 17:30:04 +0000

    Adds simple neaural network file.

---
+++ simple_neural_network.py
@@ -0,0 +1,57 @@
+# %%
+import torch
+import torch.nn.functional as F 
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import MNIST
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.datasets import load_diabetes, make_classification, load_iris
+# %%
+class DiabetesDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.X, self.y = load_diabetes(return_X_y=True)
+    
+    def __getitem__(self, index):
+        return (torch.tensor(self.X[index]).float(), torch.tensor(self.y[index]).float())
+    
+    def __len__(self):
+        return len(self.X)
+
+class NN(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # define layers
+        self.linear_layer = torch.nn.Linear(10, 16)
+        self.linear_layer2 = torch.nn.Linear(16, 1)
+
+    def forward(self, X):
+        X = self.linear_layer(X)
+        X = F.relu(X)
+        X = self.linear_layer2(X)
+        return X # return predictions
+
+def regression_train(model, train_loader, epochs=10):
+    optimiser = torch.optim.SGD(model.parameters(), lr=0.001)
+    # initialise tensorboard visualiser
+    writer = SummaryWriter()
+    batch_index = 0
+
+    for epoch in range(epochs):
+        for batch in train_loader:
+            features, labels = batch
+            prediction = model(features)
+            loss = F.mse_loss(prediction, labels)
+            loss.backward() # populates grad attributes
+            print(loss.item())
+            optimiser.step() # optimisation step
+            optimiser.zero_grad() # reset gradients
+            writer.add_scalar(tag="Loss", scalar_value=loss.item(), global_step=batch_index)
+            batch_index += 1
+# %%
+if __name__ == "__main__":
+    dataset = DiabetesDataset()
+    train_loader = DataLoader(dataset, shuffle=True, batch_size=8)
+    model = NN()
+
+    regression_train(model)
